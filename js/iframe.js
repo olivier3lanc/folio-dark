3b3990efa9aee034a7c1b1c249631de669ce7564
Olivier Blanc <olivier3lanc@gmail.com>
2021-12-09 21:18:23 +0100

    WIP

--- js/iframe.js
+++ js/iframe.js
@@ -4,6 +4,9 @@ const iframe = {
         btn_next_scene: document.querySelector('#app_next_scene'),
         btn_prev_scene: document.querySelector('#app_prev_scene'),
         scene: document.querySelector('[scroll-frames]'),
+        dark_title: document.querySelector('#app_dark_title'),
+        dark_text_split: document.querySelector('#app_dark_text_split'),
+        
     },
 
     update: function() {
@@ -21,6 +24,14 @@ const iframe = {
             const prev_scene_id = story[prev_scene_index];
             this.elements.btn_next_scene.setAttribute('href', 'scene.html?scene='+next_scene_id);
             this.elements.btn_prev_scene.setAttribute('href', 'scene.html?scene='+prev_scene_id);
+            // Texts
+            if (scene_current_id == 'opening-credits-h') {
+                this.elements.dark_title.classList.remove('u-none');
+                this.elements.dark_text_split.classList.add('u-none');
+            } else {
+                this.elements.dark_title.classList.add('u-none');
+                this.elements.dark_text_split.classList.remove('u-none');
+            }
         }
         if (this.elements.loader !== null) {
             window.addEventListener('load', function() {
